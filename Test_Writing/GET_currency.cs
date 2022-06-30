a4ee9f9903c0d17f758a9d6ba1193a008bf9205e
lyakot <95027854+lyakot@users.noreply.github.com>
2022-06-30 19:44:04 +0300

    parsing

---
+++ Test_Writing/GET_currency.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Writing
+{
+    internal class GET_currency
+    {
+
+        HttpWebRequest _request; 
+        string _currency;
+
+        public string Response { get; set; }
+
+
+        public GET_currency(string currency)
+        {
+            _currency = currency;
+        }
+
+        public void Run()
+        {
+            _request = (HttpWebRequest)WebRequest.Create(_currency);
+            _request.Method = "GET";
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
+                var stream = response.GetResponseStream();
+                if (stream != null) Response = new StreamReader(stream).ReadToEnd();
+
+            }
+
+            catch (Exception)
+            {
+
+            }
+            
+        }
+
+    } 
+}
--- Test_Writing/Program.cs
+++ Test_Writing/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Net;
+
+
+namespace Test_Writing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+        }
+
+    }
+
+}
