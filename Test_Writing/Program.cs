b1118e8b95379dcd8aed8ad1d65ada010e51c0af
lyakot <95027854+lyakot@users.noreply.github.com>
2022-07-01 13:45:11 +0300

    new lessons

--- Test_Writing/Program.cs
+++ Test_Writing/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.IO;
-using System.Net;
+using System.Collections.Generic;
+
 
 
 namespace Test_Writing
@@ -9,7 +9,14 @@ namespace Test_Writing
     {
         static void Main(string[] args)
         {
-           
+           List<string> names = new List<string>();
+
+            names.Add("Jenny");
+            names.Add("John");
+            names.Add("Jane");
+
+            string firstName = names[0];
+            Console.WriteLine(firstName);
 
         }
 
