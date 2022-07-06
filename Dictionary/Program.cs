3a171dac18c0de06172421eebe2099716da19d26
lyakot <95027854+lyakot@users.noreply.github.com>
2022-07-06 18:19:56 +0300

    up

---
+++ Dictionary/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+        
+            List<User> listUsers = new List<User>();
+
+
+            Dictionary<string, User> dictionaryUsers = new Dictionary<string, User>();
+
+            User Keand = new User("Keand", "Kenneth", "Andersen");
+            User Jimto = new User("Jimto", "Jim", "Johnsen");
+            User Larto = new User("Larto", "Lars", "Larsen");
+            User Gityv = new User("Gityv", "Gitte", "Tysen");
+            User Vitra = new User("Vira", "Vita", "Andersen");
+
+            listUsers.Add(Keand);
+            listUsers.Add(Jimto);
+            listUsers.Add(Larto);
+            listUsers.Add(Gityv);
+            listUsers.Add(Vitra);
+
+
+            dictionaryUsers.Add(Keand.UserName, Keand);
+            dictionaryUsers.Add(Jimto.UserName, Jimto);
+            dictionaryUsers.Add(Larto.UserName, Larto);
+            dictionaryUsers.Add(Gityv.UserName, Gityv); 
+            dictionaryUsers.Add(Vitra.UserName, Vitra);
+
+
+            string userName = "Larto";
+
+            //foreach (User user in listUsers)
+            //{
+            //    if (user.UserName == userName)
+            //    {
+            //        Console.WriteLine(user);
+            //        Console.WriteLine("This is she");
+            //    }
+            //}
+
+            Console.WriteLine(dictionaryUsers[userName]);
+
+
+        }
+
+
+    }
+
+
+}
+    
---
+++ Dictionary/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class User
+    {
+        
+        public string UserName { get; private set; }
+
+
+        public string FirstName { get; private set; }
+
+
+        public string LastName { get; private set; }
+
+        public User (string userName, string firstName, string lastName)
+        {
+            this.UserName = userName;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+
+        }
+
+        public override string ToString()
+        {
+            return $"First name: {FirstName} LastName: {LastName}";
+        }
+
+    }
+}
